3e1d641feea4cd3833faa3f27340dc51be070de8
Dax Wann <daxwann@gmail.com>
2019-10-04 11:07:39 -0700

    add disc02 ex 1.6

--- disc02/exercises.py
+++ disc02/exercises.py
@@ -10,8 +10,8 @@ def keep_ints(cond, n):
   """Print out all integers 1..i..n where cond(i) is true
 
   >>> def is_even(x):
-  ...   # Even numbers have remainder 0 when divided by 2.
-  ...   return x % 2 == 0
+  ...  # Even numbers have remainder 0 when divided by 2.
+  ...  return x % 2 == 0
   >>> keep_ints(is_even, 5)
   2
   4
@@ -22,3 +22,26 @@ def keep_ints(cond, n):
       print(i)
     i += 1
 
+# 1.6 Write a function similar to keep_ints like before, 
+#     but now it takes in a number n and returns a function 
+#     that has one parameter cond. The returned function prints 
+#     out numbers from 1 to n where calling cond on that number
+
+def make_keeper(n):
+  """Returns a function which takes one parameter cond and prints out
+all integers 1..i..n where calling cond(i) returns True.
+
+  >>> def is_even(x):
+  ...   # Even numbers have remainder 0 when divided by 2.
+  ...   return x % 2 == 0
+  >>> make_keeper(5)(is_even)
+  2
+  4
+  """
+  def check_cond(cond):
+    i = 1
+    while i <= n:
+      if cond(i):
+        print(i)
+      i += 1
+  return check_cond
