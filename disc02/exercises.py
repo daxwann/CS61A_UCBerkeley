4a6e2d102600bef9e53d33a1da9750094177e14c
Dax Wann <daxwann@gmail.com>
2019-10-04 10:52:35 -0700

    add disc02 ex 1.5

--- disc02/exercises.py
+++ disc02/exercises.py
@@ -2,3 +2,23 @@
 
 curry2 = lambda h: lambda x: lambda y: h(x, y)
 
+#1.5  Write a function that takes in a function cond and 
+#     a number n and prints numbers from 1 to n where 
+#     calling cond on that number returns True.
+
+def keep_ints(cond, n):
+  """Print out all integers 1..i..n where cond(i) is true
+
+  >>> def is_even(x):
+  ...   # Even numbers have remainder 0 when divided by 2.
+  ...   return x % 2 == 0
+  >>> keep_ints(is_even, 5)
+  2
+  4
+  """
+  i = 1
+  while i <= n:
+    if cond(i):
+      print(i)
+    i += 1
+
